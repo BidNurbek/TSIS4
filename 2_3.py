197076d981f68b04658f11930f4cec24eb9cc7bf
BidNurbek <156984824+BidNurbek@users.noreply.github.com>
2024-02-21 20:39:21 +0600

    2_3.py

--- 2_3.py
+++ 2_3.py
@@ -1,4 +1,9 @@
+def generator(n):
+    for i in range(n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
 n = int(input())
-a = (i for i in range(n) if i % 3 == 0 and i % 4 == 0)
-for i in a:
+x = generator(n)
+for i in x:
     print(i)
